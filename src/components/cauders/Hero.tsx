234af2e26d4ce8fe63d880a2aa52bc4a2c10a923
Muryam Naveed <muryamMN@gmail.com>
2025-08-27 02:06:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/cauders/Hero.tsx
+++ src/components/cauders/Hero.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { ArrowDown } from 'lucide-react';
 import ScrollFadeIn from './ScrollFadeIn';
-import ThreeScene from './ThreeScene';
+import IconCloud from './IconCloud';
 
 export default function Hero() {
   const scrollToServices = () => {
@@ -16,7 +16,7 @@ export default function Hero() {
   return (
     <section className="relative w-full h-screen flex flex-col items-center justify-center text-center overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full bg-background -z-10">
-        <ThreeScene />
+        <IconCloud />
       </div>
       <div className="relative z-10 container mx-auto px-4 sm:px-6 lg:px-8">
         <ScrollFadeIn>
