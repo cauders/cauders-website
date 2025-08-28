e3a2859d789ea2e3ca68f313963ae5ec01f90afa
Muryam Naveed <muryamMN@gmail.com>
2025-08-28 18:07:14 +0000

    more delay

--- src/components/cauders/Header.tsx
+++ src/components/cauders/Header.tsx
@@ -75,7 +75,7 @@ export default function Header() {
                         "text-3xl font-light",
                         isMenuOpen ? "opacity-100 animate-fade-in-down" : "opacity-0"
                     )}
-                     style={{ animationDelay: `${500 + index * 100}ms` }}
+                     style={{ animationDelay: `${500 + index * 150}ms` }}
                 >
                     <span className="menu-link-gradient">
                       {link.label}
