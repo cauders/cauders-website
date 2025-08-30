a4b210b5df70526ac4c68bf508b07d570593908e
Muryam Naveed <muryamMN@gmail.com>
2025-08-30 21:40:22 +0000

    Of this section a little above because there is a lot of space on top of

--- src/components/cauders/StickyScrollText.tsx
+++ src/components/cauders/StickyScrollText.tsx
@@ -67,7 +67,7 @@ const StickyScrollText = () => {
   }, []);
 
   return (
-    <div ref={containerRef} className="relative flex flex-col h-[600vh] bg-background">
+    <div ref={containerRef} className="relative flex flex-col h-[400vh] bg-background">
       {/* Sticky container for the animated text */}
       <div className="sticky top-0 flex-shrink-0 flex items-center justify-center overflow-hidden h-[100vh]">
         <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
