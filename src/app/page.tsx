3a3580c69c9cc081b092b73d0a750545c07630e5
Muryam Naveed <muryamMN@gmail.com>
2025-08-31 00:22:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-
-import Hero from '@/components/cauders/Hero';
-import PortfolioSection from '@/components/cauders/PortfolioSection';
-import Contact from '@/components/cauders/Contact';
-import StickyScrollText from '@/components/cauders/StickyScrollText';
-import ServicesPreview from '@/components/cauders/ServicesPreview';
+import HomePage from '@/components/cauders/HomePage';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <StickyScrollText />
-      <ServicesPreview />
-      <PortfolioSection />
-      <Contact />
-    </>
-  );
+  return <HomePage />;
 }
---
+++ src/components/cauders/HomePage.tsx
@@ -0,0 +1,20 @@
+
+"use client";
+
+import Hero from '@/components/cauders/Hero';
+import PortfolioSection from '@/components/cauders/PortfolioSection';
+import Contact from '@/components/cauders/Contact';
+import StickyScrollText from '@/components/cauders/StickyScrollText';
+import ServicesPreview from '@/components/cauders/ServicesPreview';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <StickyScrollText />
+      <ServicesPreview />
+      <PortfolioSection />
+      <Contact />
+    </>
+  );
+}
