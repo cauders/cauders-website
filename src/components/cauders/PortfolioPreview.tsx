2cf4fb62da18ec23b8ecf443bd6246679b393246
Muryam Naveed <muryamMN@gmail.com>
2025-09-14 23:22:05 +0000

    This potential lead to the subdomain portfolio (_for element <Button>_)

--- src/components/cauders/PortfolioPreview.tsx
+++ src/components/cauders/PortfolioPreview.tsx
@@ -238,7 +238,7 @@ export default function PortfolioPreview() {
             variant="outline"
             className="border-primary text-primary glass-effect hover:shadow-primary-glow"
           >
-            <Link href="/portfolio">
+            <Link href="https://www.portfolio.cauders.com/">
               See All Projects <ArrowRight className="ml-2" />
             </Link>
           </Button>
