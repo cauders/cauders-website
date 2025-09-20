a791fa3c895a7cd4c92a2504954ce7082edf00c4
Muryam Naveed <muryamMN@gmail.com>
2025-09-20 04:05:33 +0000

    Give all the cards a dark background and on however we can use primary c

--- src/lib/team-data.ts
+++ src/lib/team-data.ts
@@ -75,7 +75,7 @@ export const teamMembers: TeamMember[] = [
 ];
 
 const borderColor = "#8CEAE5";
-const gradient = "hsl(var(--primary))";
+const gradient = "hsl(var(--card))";
 
 
 export const teamData = teamMembers.map((member, index) => ({
