92dc948d762cc742904ebcc859a934a843b20101
Muryam Naveed <muryamMN@gmail.com>
2025-09-20 04:34:34 +0000

    There is no magnetic effect please look into this matter

--- src/hooks/useMagneticEffect.ts
+++ src/hooks/useMagneticEffect.ts
@@ -3,7 +3,7 @@
 
 import { useState, useEffect, useRef, type RefObject } from 'react';
 
-const LERP_FACTOR = 0.2; 
+const LERP_FACTOR = 0.1; 
 const MAGNET_RADIUS = 80;
 
 export function useMagneticEffect(ref: RefObject<HTMLElement>) {
