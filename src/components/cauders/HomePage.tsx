6fa5107b21c357b0a0f3634ac249619a590c310e
Muryam Naveed <muryamMN@gmail.com>
2025-10-14 18:49:10 +0000

    Let's redesign our home page for now remove everything from the home pag

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -1,24 +1,9 @@
-
 "use client";
-
-import Hero from '@/components/cauders/Hero';
-import PortfolioSection from '@/components/cauders/PortfolioSection';
-import StickyScrollText from '@/components/cauders/StickyScrollText';
-import ServicesPreview from '@/components/cauders/ServicesPreview';
-import Testimonials from './Testimonials';
-import ScrollFadeIn from './ScrollFadeIn';
-import { Button } from '../ui/button';
-import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
-      <StickyScrollText />
-      <ServicesPreview />
-      <PortfolioSection />
-      <Testimonials />
+      {/* The home page is cleared for redesign. */}
     </>
   );
 }
