d7af3897ad272cd3f47ace4b33de65e587c99e11
Muryam Naveed <muryamMN@gmail.com>
2025-10-14 20:13:33 +0000

    Make the description only two lines like you can remove some words. (_fo

--- src/components/cauders/Hero.tsx
+++ src/components/cauders/Hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
                 />
               <FadeContent delay={1000}>
                 <p className="mt-6 text-xs text-white max-w-md font-light">
-                    At Cauders, we architect and engineer premium, modern, and dynamic digital solutions that empower businesses to thrive in a competitive landscape.
+                    At Cauders, we architect and engineer premium, modern, and dynamic digital solutions that empower businesses to thrive.
                 </p>
               </FadeContent>
               <FadeContent delay={1200}>
