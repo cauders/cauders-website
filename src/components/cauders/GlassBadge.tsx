7df9106c0d3dc267dd56611d509c5533482eae94
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 18:13:28 +0000

    i'll put a file in images.

--- src/components/cauders/GlassBadge.tsx
+++ src/components/cauders/GlassBadge.tsx
@@ -24,23 +24,12 @@ export default function GlassBadge({ href, text, className }: GlassBadgeProps) {
       <svg
         width="12"
         height="12"
-        viewBox="0 0 12 12"
+        viewBox="0 0 8 12"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className="opacity-75"
       >
-        <path
-          d="M6 0V12"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-        <path
-          d="M12 6L0 6"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
+        <path d="M1 1L6 6L1 11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
       <span>{text}</span>
     </Link>
