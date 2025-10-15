5ad4d58ed943b5a7a418f911b40d4ac450e86eb9
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 18:30:56 +0000

    Currently the background of HOME PAGE is white. replace it with an image

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -7,17 +7,21 @@ import Testimonials from './Testimonials';
 import StickyScrollText from './StickyScrollText';
 import Contact from './Contact';
 import Footer from './Footer';
+import HomePageBackground from './HomePageBackground';
 
 export default function HomePage() {
   return (
-    <>
-      <Hero />
-      <ServicesPreview />
-      <PortfolioSection />
-      <Testimonials />
-      <StickyScrollText />
-      <Contact />
-      <Footer />
-    </>
+    <div className="relative">
+      <HomePageBackground />
+      <div className="relative z-10">
+        <Hero />
+        <ServicesPreview />
+        <PortfolioSection />
+        <Testimonials />
+        <StickyScrollText />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
   );
 }
