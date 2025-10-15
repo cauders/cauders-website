8bad884e439f252359ccfdaa94fd214ee1c1f49d
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 18:47:45 +0000

    Where the **** did you edit thatI will create a new component named Intr

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Hero from '@/components/cauders/Hero';
+import IntroSection from '@/components/cauders/IntroSection';
 import ServicesPreview from '@/components/cauders/ServicesPreview';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <IntroSection />
       <ServicesPreview />
     </>
   );
