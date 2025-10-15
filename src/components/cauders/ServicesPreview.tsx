5040cde636c9dac8824d65cdddb80c2e3b36631e
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 21:20:32 +0000

    there must be equal verti and horiz gap. as we reduced width so u need t

--- src/components/cauders/ServicesPreview.tsx
+++ src/components/cauders/ServicesPreview.tsx
@@ -27,10 +27,10 @@ export default function ServicesPreview() {
               </ScrollFadeIn>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-0 gap-y-8 mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 mx-auto">
             {services.map((service, index) => (
               <ScrollFadeIn key={service.slug} style={{ animationDelay: `${index * 100}ms` }} className="h-full">
-                  <Card className="h-full bg-card border flex flex-col hover:border-primary hover:-translate-y-2 transition-transform duration-300 group p-6 text-left rounded-2xl mx-3">
+                  <Card className="h-full bg-card border flex flex-col hover:border-primary hover:-translate-y-2 transition-transform duration-300 group p-6 text-left rounded-2xl">
                       <CardHeader className="p-0 flex-row justify-start">
                           <div className="flex-shrink-0 bg-zinc rounded-full p-3 border border-primary/20 group-hover:bg-zinc/90 transition-colors w-fit">
                               <service.icon className="w-6 h-6 text-white" />
