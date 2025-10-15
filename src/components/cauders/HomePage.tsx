b54b6c93c24ce3ccff8961fc802182fcf3e2b9b1
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 20:30:17 +0000

    no bg visible

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -2,13 +2,11 @@
 
 import Hero from '@/components/cauders/Hero';
 import WhyChooseUs from './WhyChooseUs';
-import HomePageBackground from './HomePageBackground';
 import ServicesPreview from './ServicesPreview';
 
 export default function HomePage() {
   return (
     <>
-      <HomePageBackground />
       <Hero />
       <WhyChooseUs />
       <ServicesPreview />
