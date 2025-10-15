c716722b37ec42e5f153048826c9bc5a9ee4fbb0
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 18:44:59 +0000

    ??

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -2,26 +2,12 @@
 
 import Hero from '@/components/cauders/Hero';
 import ServicesPreview from '@/components/cauders/ServicesPreview';
-import PortfolioSection from './PortfolioSection';
-import Testimonials from './Testimonials';
-import StickyScrollText from './StickyScrollText';
-import Contact from './Contact';
-import Footer from './Footer';
-import HomePageBackground from './HomePageBackground';
 
 export default function HomePage() {
   return (
-    <div className="relative">
-      <HomePageBackground />
-      <div className="relative z-10">
-        <Hero />
-        <ServicesPreview />
-        <PortfolioSection />
-        <Testimonials />
-        <StickyScrollText />
-        <Contact />
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Hero />
+      <ServicesPreview />
+    </>
   );
 }
