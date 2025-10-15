9884cb2e6347d23920c901d4cf6373d9f3c756e8
Muryam Naveed <muryamMN@gmail.com>
2025-10-15 19:06:27 +0000

    a litttleee more

--- src/components/cauders/IntroSection.tsx
+++ src/components/cauders/IntroSection.tsx
@@ -9,7 +9,7 @@ export default function IntroSection() {
     const { intro: introImage } = placeholderImages;
 
     return (
-        <div className="relative z-10 pt-8 -mt-16">
+        <div className="relative z-10 pt-8">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="rounded-2xl shadow-xl overflow-hidden bg-card">
                     <div className="text-center pt-16 pb-12">
