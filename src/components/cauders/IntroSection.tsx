4dee7a1fe8483af4e55bbbbc4ea0e9da4a8168f7
Muryam Naveed <muryamMN@gmail.com>
2025-10-16 20:42:14 +0000

    move it here on right side (_for element <IntroSection>_)

--- src/components/cauders/IntroSection.tsx
+++ src/components/cauders/IntroSection.tsx
@@ -11,7 +11,7 @@ export default function IntroSection() {
     return (
         <div className="relative z-10 pt-8 px-4 sm:px-6 lg:px-8 pb-16">
             <div className="rounded-2xl shadow-xl overflow-hidden bg-card relative">
-                <BlurCircle className="w-96 h-96 top-1/2 -translate-y-1/2 right-[-10rem]" />
+                <BlurCircle className="w-96 h-96 top-1/2 -translate-y-1/2 right-0" />
                 <div className="text-center py-32 relative z-10">
                     <StandardizedHeading 
                         lines={[
