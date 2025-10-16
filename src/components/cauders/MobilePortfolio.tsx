d3418cb777c38576014e99d80044d3f463013d52
Muryam Naveed <muryamMN@gmail.com>
2025-10-16 00:27:12 +0000

    reduce space between both.

--- src/components/cauders/MobilePortfolio.tsx
+++ src/components/cauders/MobilePortfolio.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Image from 'next/image';
@@ -25,7 +24,7 @@ export default function MobilePortfolio() {
 
         <div className="relative mt-16">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-4">
                 <div className="bg-gradient-container rounded-2xl p-6 flex flex-col justify-between text-white min-h-[200px]">
                     <div>
                         <p className="text-base max-w-[80%]">We design seamless mobile experiences that drive results.</p>
