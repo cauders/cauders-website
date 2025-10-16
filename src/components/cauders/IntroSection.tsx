5a0cb1afd013e8b10846b33d796b6b51a25762a7
Muryam Naveed <muryamMN@gmail.com>
2025-10-16 20:42:47 +0000

    place one at bottom left corner

--- src/components/cauders/IntroSection.tsx
+++ src/components/cauders/IntroSection.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import StandardizedHeading from "./StandardizedHeading";
@@ -12,6 +13,7 @@ export default function IntroSection() {
         <div className="relative z-10 pt-8 px-4 sm:px-6 lg:px-8 pb-16">
             <div className="rounded-2xl shadow-xl overflow-hidden bg-card relative">
                 <BlurCircle className="w-96 h-96 top-1/2 -translate-y-1/2 right-0" />
+                <BlurCircle className="w-96 h-96 bottom-[-10rem] left-[-10rem]" />
                 <div className="text-center py-32 relative z-10">
                     <StandardizedHeading 
                         lines={[
