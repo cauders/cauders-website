4437c4664d328c70ecc1e2de7aa7b2fd6099d320
Muryam Naveed <muryamMN@gmail.com>
2025-10-16 00:16:24 +0000

    yes good. increase height just a little.

--- src/components/cauders/MobilePortfolio.tsx
+++ src/components/cauders/MobilePortfolio.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Image from 'next/image';
@@ -24,7 +25,7 @@ export default function MobilePortfolio() {
         <div className="relative mt-16">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="flex flex-col gap-8">
-                <div className="bg-gradient-container rounded-2xl p-6 flex flex-col justify-between text-white">
+                <div className="bg-gradient-container rounded-2xl p-6 flex flex-col justify-between text-white min-h-[200px]">
                     <div>
                         <p className="text-base">We design seamless mobile experiences that drive engagement and results.</p>
                     </div>
