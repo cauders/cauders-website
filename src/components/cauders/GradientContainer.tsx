0a2086a8c42361d44f997ed29491b8d955f761ae
Muryam Naveed <muryamMN@gmail.com>
2025-10-16 01:54:43 +0000

    do bottom -60

--- src/components/cauders/GradientContainer.tsx
+++ src/components/cauders/GradientContainer.tsx
@@ -18,7 +18,7 @@ export default function GradientContainer({ children, className }: GradientConta
         alt="background overlay"
         width={1920}
         height={1080}
-        className="object-cover absolute w-full h-auto bottom-[-40px] left-0 right-0 z-0"
+        className="object-cover absolute w-full h-auto bottom-[-60px] left-0 right-0 z-0"
       />
       <div className="relative z-10">
         {children}
@@ -26,3 +26,4 @@ export default function GradientContainer({ children, className }: GradientConta
     </div>
   );
 }
+
