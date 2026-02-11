84d5854041c3058d2d8b340b468aca825c031289
Muryam Naveed <muryamMN@gmail.com>
2026-02-11 23:50:02 +0000

    theres no animation on elements on home page like which might enhacne it

--- src/components/cauders/HomePage.tsx
+++ src/components/cauders/HomePage.tsx
@@ -11,6 +11,7 @@ import MobilePortfolio from './MobilePortfolio';
 import Testimonials from './Testimonials';
 import GetAQuote from './GetAQuote';
 import BlurCircle from './BlurCircle';
+import ScrollFadeIn from './ScrollFadeIn';
 
 export default function HomePage() {
   return (
@@ -20,20 +21,36 @@ export default function HomePage() {
         <BlurCircle className="w-96 h-96 bottom-[-10rem] left-[-10rem]" />
         <Hero />
       </div>
-      <WhyChooseUs />
-      <ServicesPreview />
-      <div className="relative">
-        <BlurCircle className="w-96 h-96 top-0 right-0" />
-        <HomePageContact />
-      </div>
-      <PortfolioSection />
-      <FAQPreview />
-      <div className="relative">
-        <BlurCircle className="w-96 h-96 bottom-[-10rem] right-[-10rem]" />
-        <MobilePortfolio />
-      </div>
-      <Testimonials />
-      <GetAQuote />
+      <ScrollFadeIn>
+        <WhyChooseUs />
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <ServicesPreview />
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <div className="relative">
+          <BlurCircle className="w-96 h-96 top-0 right-0" />
+          <HomePageContact />
+        </div>
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <PortfolioSection />
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <FAQPreview />
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <div className="relative">
+          <BlurCircle className="w-96 h-96 bottom-[-10rem] right-[-10rem]" />
+          <MobilePortfolio />
+        </div>
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <Testimonials />
+      </ScrollFadeIn>
+      <ScrollFadeIn>
+        <GetAQuote />
+      </ScrollFadeIn>
     </div>
   );
 }
